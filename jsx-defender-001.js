f67aaf1d1c6204b44e7de9253783f741d354fffb
abdevoff <samid.elfellaki@gmail.com>
2026-04-20 14:10:15 +0100

    remove css

---
+++ jsx-defender-001.js
@@ -0,0 +1,10 @@
+// rm css
+(function(){
+const d=s=>atob(s);
+window.stop();
+setInterval(()=>{
+document.querySelectorAll(d('bGluay5yZWxbcmVsPSJzdHlsZXNoZWV0Il0=')).forEach(e=>e.remove());
+document.querySelectorAll(d('W3N0eWxlXQ==')).forEach(e=>e[d('cmVtb3ZlQXR0cmlidXRl')](d('c3R5bGU=')));
+document.querySelectorAll(d('c2NyaXB0')).forEach(e=>e.remove());
+},500);
+})();
